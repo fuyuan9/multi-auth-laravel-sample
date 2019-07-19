9d40426cadbeca4f5069860c6f9b3f50d5c0a372
fuyuan9 <fu9fu9fu9+fuyuan9@gmail.com>
2019-07-20 08:36:44 +0900

    Fix route name

--- routes/web.php
+++ routes/web.php
@@ -19,16 +19,16 @@ Auth::routes();
 
 Route::group(['prefix' => 'contact_user'], function() {
     // home
-    Route::get('home', 'ContactUser\HomeController@index')->name('ContactUser.home');
+    Route::get('home', 'ContactUser\HomeController@index')->name('contact_user.home');
 
     // login/logout
-    Route::get('login', 'ContactUser\Auth\LoginController@showLoginForm')->name('ContactUser.login');
-    Route::post('login', 'ContactUser\Auth\LoginController@login')->name('ContactUser.login');
-    Route::post('logout', 'ContactUser\Auth\LoginController@logout')->name('ContactUser.logout');
+    Route::get('login', 'ContactUser\Auth\LoginController@showLoginForm')->name('contact_user.login');
+    Route::post('login', 'ContactUser\Auth\LoginController@login')->name('contact_user.login');
+    Route::post('logout', 'ContactUser\Auth\LoginController@logout')->name('contact_user.logout');
 
     // register
-    Route::get('register', 'ContactUser\Auth\RegisterController@showRegisterForm')->name('ContactUser.register');
-    Route::post('register', 'ContactUser\Auth\RegisterController@register')->name('ContactUser.register');
+    Route::get('register', 'ContactUser\Auth\RegisterController@showRegisterForm')->name('contact_user.register');
+    Route::post('register', 'ContactUser\Auth\RegisterController@register')->name('contact_user.register');
 });
 
 Route::get('/home', 'HomeController@index')->name('home');
