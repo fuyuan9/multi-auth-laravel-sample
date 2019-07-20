0bc5c924067c487f108a7a0e6a6d6e9f6c3cb790
fuyuan9 <fu9fu9fu9+fuyuan9@gmail.com>
2019-07-20 09:04:49 +0900

    Fix views

--- resources/views/contact_user/auth/register.blade.php
+++ resources/views/contact_user/auth/register.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.contact_user')
 
 @section('content')
 <div class="container">
@@ -26,12 +26,12 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="uid" class="col-md-4 col-form-label text-md-right">ID</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                <input id="uid" type="text" class="form-control @error('uid') is-invalid @enderror" name="uid" value="{{ old('uid') }}" required>
 
-                                @error('email')
+                                @error('uid')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
